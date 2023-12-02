16ade8cad22c37ddb8a389b43ada22442a659eed
meesam <meesam.engineer@gmail.com>
2023-12-02 20:19:15 +0530

    add city load method using javascript

---
+++ Blazor-AuthorApp/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+function saveSuccess(){
+    alert("Author added Successfully.")
+}
+
+function loadCities(){
+    return ["Noida", "New Delhi", "Gurgaon", "Mumbai", "Bihar", "Hydrabad", "Lucknow", "Kanpur"]
+}
+
